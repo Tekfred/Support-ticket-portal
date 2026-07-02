5344858fbb234e3f8e2d2bc3eb3979bfd138b4cb
Tekfred <asagodfred101@gmail.com>
2026-07-02 11:08:08 +0000

    feat: myworkspace background state change

--- src/views/MyWorkspacePage.vue
+++ src/views/MyWorkspacePage.vue
@@ -45,7 +45,7 @@ function handleReleaseTicket(id) {
 </script>
 
 <template>
-  <div class="flex-1 min-h-screen bg-slate-50 p-6 md:p-8 overflow-y-auto font-sans">
+  <div class="flex-1 min-h-screen p-6 md:p-8 overflow-y-auto font-sans">
     <Transition name="fade-slide" mode="out-in">
 
       <!-- LIST VIEW -->
